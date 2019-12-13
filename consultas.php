9d33b5c894cbb4cb65bb5fbef3123814d5b0fe52
aandresaarias <aandresaarias@hotmail.com>
2019-12-13 16:41:11 -0500

    funcional, consulta desde form

--- consultas.php
+++ consultas.php
@@ -1,10 +1,47 @@
 <?php
 include('conn.php');
-	
-	$idCuadernillo = ($_GET['idCuadernillo']);
-	$idCuadernillo2 = 'S03M03N2';
-	
+
+$idCuadernillo = $_POST['idCuadernillo'];
+if ($conn)
+		{
+			$consultaListaCuadernillos = "
+			SELECT
+			   f412_id_ubicacion_aux
+			   ,f410_consecutivo
+			   ,f412_numero_etiqueta
+			   ,f120_descripcion
+			  FROM [t412_cm_control_fisico_movto]
+			  INNER JOIN [t410_cm_control_fisico] ON [dbo].[t410_cm_control_fisico].[f410_rowid] = [t412_cm_control_fisico_movto].[f412_rowid_cntrl_fisico]
+			  INNER JOIN [t120_mc_items] ON [t120_mc_items].[f120_rowid] = [t412_cm_control_fisico_movto].[f412_rowid_item_ext]
+			  where f410_rowid = 18
+			  and f412_id_ubicacion_aux = '$idCuadernillo'";
+
+	$consultaListaPorCuadernilloTabla = sqlsrv_query($conn, $consultaListaCuadernillos);
 		
+		if ($consultaListaPorCuadernilloTabla == FALSE)
+			die(FormatErrors(sqlsrv_errors()));
+	
+		while
+			($row = sqlsrv_fetch_array($consultaListaPorCuadernilloTabla, SQLSRV_FETCH_ASSOC))
+		{
+			echo ($row['f412_id_ubicacion_aux']
+				  ." | ".
+				   $row['f410_consecutivo']
+				  ." | ".
+				   $row['f412_numero_etiqueta']
+				  ." | ".
+	   			   $row['f120_descripcion']
+				  ." | <br />");
+		}
+	
+		sqlsrv_free_stmt ($consultaListaPorCuadernilloTabla);
+		}
+		else
+		{
+			echo("no se conectó a la bd" + $connInfo["Database"]);
+			die (print_r(sqlsrv_errors(), true));
+		}
+	
 	/*
 	$consultaCuadernillos = sqlsrv_query($conn, "
 	SELECT
@@ -15,24 +52,18 @@ include('conn.php');
 	  INNER JOIN [t120_mc_items] ON [t120_mc_items].[f120_rowid] = [t412_cm_control_fisico_movto].[f412_rowid_item_ext]
 	  where f410_rowid = '18'");
 	  */
-	
-	//$idCuadernillo2= S03M03N2;
-	$consultaListaCuadernillos = "
-	SELECT
-       f412_id_ubicacion_aux
-	   ,f410_consecutivo
-	   ,f412_numero_etiqueta
-	   ,f120_descripcion
-	  FROM [t412_cm_control_fisico_movto]
-	  INNER JOIN [t410_cm_control_fisico] ON [dbo].[t410_cm_control_fisico].[f410_rowid] = [t412_cm_control_fisico_movto].[f412_rowid_cntrl_fisico]
-	  INNER JOIN [t120_mc_items] ON [t120_mc_items].[f120_rowid] = [t412_cm_control_fisico_movto].[f412_rowid_item_ext]
-	  where f410_rowid = 18
-	  and f412_id_ubicacion_aux = '$idCuadernillo2'";
-	
-	$consultaListaPorCuadernilloTabla = sqlsrv_query($conn, $consultaListaCuadernillos);
+		
+		function FormatErrors( $errors )
+		{
+			/* Display errors. */
+			echo "Error information: ";
 
-	//echo $consultaCuadernillos;
-	//echo $consultaListaCuadernillos;
-	echo $consultaListaPorCuadernilloTabla;
-  	
+			foreach ( $errors as $error )
+			{
+				echo "SQLSTATE: ".$error['SQLSTATE']."";
+				echo "Code: ".$error['code']."";
+				echo "Message: ".$error['message']."";
+			}
+		}
+
 ?>
--- index.php
+++ index.php
@@ -1,51 +1,7 @@
 <html>
 	<body>
-	<?php
-	include 'conn.php';
-	include 'consultas.php';
-	
-	if ($conn)
-		{
-			echo("conexion establecida a BD " . $connInfo["Database"] . ".<br />");
-		}
-		else
-		{
-			echo("no se conectó a la bd" + $connInfo["Database"]);
-			die (print_r(sqlsrv_errors(), true));
-		}
-		
-		if ($consultaListaPorCuadernilloTabla == FALSE)
-			die(FormatErrors(sqlsrv_errors()));
-		while
-			($row = sqlsrv_fetch_array($consultaListaPorCuadernilloTabla, SQLSRV_FETCH_ASSOC))
-		{
-			echo ($row['f412_id_ubicacion_aux']
-				  ." | ".
-				   $row['f410_consecutivo']
-				  ." | ".
-				   $row['f412_numero_etiqueta']
-				  ." | ".
-	   			   $row['f120_descripcion']
-				  ." | <br />");
-		}
-	
-		sqlsrv_free_stmt ($consultaListaPorCuadernilloTabla);
-		
-		function FormatErrors( $errors )
-		{
-			/* Display errors. */
-			echo "Error information: ";
-
-			foreach ( $errors as $error )
-			{
-				echo "SQLSTATE: ".$error['SQLSTATE']."";
-				echo "Code: ".$error['code']."";
-				echo "Message: ".$error['message']."";
-			}
-		}
-	?>
 			
-	<form name = "consulta" action = consultas.php method="get">
+	<form name = "consulta" action = consultas.php method="post">
 		<input type = "text" name = "idCuadernillo" required ="no debe estar en blanco">
 		<input type="submit" value = "cargar">
   	</form
